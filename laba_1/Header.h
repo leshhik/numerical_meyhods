97c9e425814843c954fcf466be7ab954403454cb
Alex Varvashevich <146416031+leshhik@users.noreply.github.com>
2023-10-24 22:10:14 +0300

    Update Header1.h

--- laba_1/Header.h
+++ laba_1/Header.h
@@ -1,1 +1,9 @@
-#pragma once
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void printMatrix(double** matrix, double* vector, int size);
+double* metodGauss(double** a, double* b, int n);
+double* metodGauss2(double** a, double* F, int n);
+
