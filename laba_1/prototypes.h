c4ab3836b89e7b43f7f934fb2eec7dbee3c5bcc7
Alex Varvashevich <146416031+leshhik@users.noreply.github.com>
2023-11-14 01:55:53 +0300

    Update list of prototypes. I missed the "void normalizeRow". Include it and rename Source.h to prototypes.h
    the 14-s row: 
    void normalizeRow(double* row, double divisor, int size);

--- laba_1/prototypes.h
+++ laba_1/prototypes.h
@@ -6,9 +6,9 @@ using namespace std;
 void readMatrix(double** matrix, int size);
 void readVector(double* vector, int size);
 void printMatrix(double** matrix, double* vector, int size);
-double* gaussianElimination(double** matrix, double* vector, int size);
+double* gaussSolution(double** matrix, double* vector, int size);
 double calculateRelativeError(double* residual, double* solution, int size);
 void printSolution(double* solution, int size);
 void printResidualVector(double** matrix, double* vector, double* solution, int size);
 double calculateNorm(double* vector, int size);
-
+void normalizeRow(double* row, double divisor, int size);
